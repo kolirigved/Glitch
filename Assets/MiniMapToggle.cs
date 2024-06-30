a3cfb7fa0806deb0f061fe9d80e0f921d872ff57
APOLCLP <amrj0880@gmail.com>
2024-07-01 00:50:12 +0530

    Update MiniMapToggle.cs

--- Assets/MiniMapToggle.cs
+++ Assets/MiniMapToggle.cs
@@ -7,6 +7,14 @@ public class MiniMapToggle : MonoBehaviour
     public GameObject RawImage; // Assign the minimap GameObject in the Inspector
     private bool isMinimized = false;
 
+    void Update()
+    {
+        // Check if the "M" key is pressed
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ToggleMinimap();
+        }
+    }
     public void ToggleMinimap()
     {
         isMinimized = !isMinimized;
